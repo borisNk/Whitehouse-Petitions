7da153605a66cb94cc9dc4d5158b529f9da664e0
borisNk <borisnk@icloud.com>
2020-02-23 21:36:20 +0100

    Initial Commit

---
+++ Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Boris Nikolaev Borisov on 23/02/2020.
+//  Copyright © 2020 Boris Nikolaev Borisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
